fa74a216b8df60da85934afcc42672df62a3a94b
Aditya <icgadi99@gmail.com>
2020-07-22 18:18:52 +0530

    Added Bottom Nav, Optimized XML Codes

--- app/src/main/java/com/dsckiet/covidtracker/PendingListAdapter.kt
+++ app/src/main/java/com/dsckiet/covidtracker/PendingListAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_view.view.*
 
@@ -21,6 +24,13 @@ class PendingListAdapter(ctx: Context, private val listItem: List<TempDataClass>
         holder.patientId.text = currentItem.patientId
         holder.patientAge.text = currentItem.patientAge.toString()
         holder.patientName.text = currentItem.patientName
+        holder.patientCard.setOnClickListener {
+            val patientId = holder.patientId.text.toString()
+            val patientName = holder.patientName.text.toString()
+            val patientAge = holder.patientAge.text.toString()
+            it.findNavController().navigate(R.id.action_diagnosisPendingFragment_to_patientDetailsFragment,
+                bundleOf(patientId to "id", patientAge to "age", patientName to "name"))
+        }
     }
 
     override fun getItemCount(): Int = listItem.size
@@ -29,5 +39,6 @@ class PendingListAdapter(ctx: Context, private val listItem: List<TempDataClass>
         val patientId: TextView = unitView.patient_id
         val patientName: TextView = unitView.patient_name
         val patientAge: TextView = unitView.patient_age
+        val patientCard: CardView = unitView.patient_card
     }
 }
