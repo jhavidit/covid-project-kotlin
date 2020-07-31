f112b0af2ca72dc8b729829de38642589d5a46ae
Vidit Jha <jhavidit@gmail.com>
2020-07-31 12:03:04 +0530

    Profile update

---
+++ app/src/main/java/com/dsckiet/covidtracker/screens/ui/ProfileUpdateActivity.kt
@@ -0,0 +1,11 @@
+package com.dsckiet.covidtracker.screens.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_update)
+    }
+}
