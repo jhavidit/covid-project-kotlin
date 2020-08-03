b66ac6d2cfa64670d27d8227a3dc4092a92c90ef
Anshul1507 <ag1507anshul@gmail.com>
2020-08-03 19:39:47 +0530

    Refactored - Fixed Diagosis Pending layout, internet checks

--- app/src/main/java/com/dsckiet/covidtracker/screens/ui/DiagnosisPendingFragment.kt
+++ app/src/main/java/com/dsckiet/covidtracker/screens/ui/DiagnosisPendingFragment.kt
@@ -1,7 +1,8 @@
 package com.dsckiet.covidtracker.screens.ui
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -19,12 +20,12 @@ import com.dsckiet.covidtracker.model.PendingPatient
 import com.dsckiet.covidtracker.network.SocketInstance
 import com.dsckiet.covidtracker.screens.adapters.DiagnosisPendingAdapter
 import com.dsckiet.covidtracker.utils.InternetConnectivity
+import com.dsckiet.covidtracker.utils.logs
 import com.github.nkzawa.socketio.client.IO
 import com.github.nkzawa.socketio.client.Socket
 import com.google.android.material.snackbar.Snackbar
 import com.robinhood.ticker.TickerUtils
 import com.robinhood.ticker.TickerView
-import kotlinx.android.synthetic.main.fragment_diagnosis_pending.*
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -36,7 +37,7 @@ class DiagnosisPendingFragment : Fragment() {
     private var patientData: JSONArray? = null
     private lateinit var tokenManager: TokenManager
     private lateinit var list: ArrayList<PendingPatient>
-    private lateinit var tickerView : TickerView
+    private lateinit var tickerView: TickerView
 
     private lateinit var binding: FragmentDiagnosisPendingBinding
     override fun onCreateView(
@@ -45,8 +46,10 @@ class DiagnosisPendingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding =
-            DataBindingUtil.inflate(inflater,
-                R.layout.fragment_diagnosis_pending, container, false)
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_diagnosis_pending, container, false
+            )
         tickerView = binding.diagnosisPendingCount
         tickerView.animationInterpolator = OvershootInterpolator()
         tickerView.setCharacterLists(TickerUtils.provideNumberList())
@@ -55,21 +58,33 @@ class DiagnosisPendingFragment : Fragment() {
         tickerView.typeface = fontFace
         tickerView.gravity = Gravity.START
         tickerView.animationDuration = 2000
+
+        binding.openSettings.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
+        }
         return binding.root
     }
 
-    @SuppressLint("LogNotTimber")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tokenManager = TokenManager(requireContext())
-        if(!InternetConnectivity.isNetworkAvailable(requireContext())!!) {
-            Snackbar.make(binding.coordinatorLayout,"Internet Unavailable",Snackbar.LENGTH_SHORT).show()
-            binding.animationView.visibility=GONE
+        if (!InternetConnectivity.isNetworkAvailable(requireContext())!!) {
+            offlineCase()
         }
     }
+
+    private fun offlineCase() {
+        binding.animationView.setAnimation(R.raw.no_internet)
+        binding.animationView.visibility = VISIBLE
+        binding.openSettings.visibility = VISIBLE
+        Snackbar.make(binding.coordinatorLayout, "Network Problem", Snackbar.LENGTH_LONG)
+            .setAction("Open Settings") {
+                startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
+            }.show()
+    }
+
     private fun generatePendingPatientList(data: JSONArray): ArrayList<PendingPatient> {
         val list = ArrayList<PendingPatient>()
-
 
         for (i in 0 until data.length()) {
             try {
@@ -83,7 +98,6 @@ class DiagnosisPendingFragment : Fragment() {
                     data.getJSONObject(i).getString("lab"),
                     data.getJSONObject(i).getString("district"),
                     data.getJSONObject(i).getString("caseId")
-
                 )
             } catch (e: JSONException) {
                 println("json exception : ${e.message}")
@@ -102,30 +116,36 @@ class DiagnosisPendingFragment : Fragment() {
         options.reconnection = true
         options.forceNew = true
 
-        socketConnection()
+        startSocketConnection()
     }
 
-    private fun socketConnection() {
-        val token=tokenManager.getAuthToken()
-        val jsonObject1 = JSONObject() //token object
+    private fun startSocketConnection() {
+        val token = tokenManager.getAuthToken()
+        val jsonObjectToken = JSONObject() //token object
         try {
-            jsonObject1.put(
+            jsonObjectToken.put(
                 "token",
-                token )
+                token
+            )
 
         } catch (e: JSONException) {
             e.printStackTrace()
         }
+
         mSocket!!.on("PATIENTS_POOL_FOR_DOCTOR") { args ->    //requesting patient details
             if (args[0] != null)
                 data = args[0] as JSONObject
 
             if (data != null) {
+                //to check size of data
+                logs(data!!.length().toString())
                 if (data.toString() != "{}" || data.toString() != "[]") {
                     patientData = data!!.getJSONArray("patients")
                     list = generatePendingPatientList(patientData!!)
                     activity?.runOnUiThread {
+
                         binding.animationView.visibility = GONE
+                        binding.openSettings.visibility = GONE
                         binding.recyclerView.visibility = VISIBLE
 
                         tickerView.text = data!!.getString("remainingPatients")
@@ -136,9 +156,17 @@ class DiagnosisPendingFragment : Fragment() {
                             )
                     }
                 } else {
+                    //TODO:: Need Testing of empty list data
                     activity?.runOnUiThread {
-                        binding.animationView.visibility=GONE
-                        Snackbar.make(coordinator_layout,"No available Patient",Snackbar.LENGTH_SHORT)
+                        binding.animationView.setAnimation(R.raw.empty_list)
+                        binding.animationView.visibility = VISIBLE
+                        binding.openSettings.visibility = GONE
+
+                        Snackbar.make(
+                            binding.coordinatorLayout,
+                            "No available Patient",
+                            Snackbar.LENGTH_LONG
+                        )
                     }
                 }
             }
@@ -146,19 +174,38 @@ class DiagnosisPendingFragment : Fragment() {
         }.on(Socket.EVENT_DISCONNECT) {
             activity?.runOnUiThread {
                 binding.recyclerView.visibility = GONE
-                binding.animationView.visibility = VISIBLE
+
+                if (InternetConnectivity.isNetworkAvailable(requireContext()) == false) {
+                    offlineCase()
+                } else {
+                    binding.animationView.setAnimation(R.raw.heartbeat_loading)
+                    binding.animationView.visibility = VISIBLE
+                    binding.openSettings.visibility = GONE
+                }
+
                 tickerView.text = "0"
-                Snackbar.make(binding.coordinatorLayout,"Some problem occurred check your network connection or restart the app",Snackbar.LENGTH_SHORT).show()
+                Snackbar.make(binding.coordinatorLayout, "Network Problem", Snackbar.LENGTH_LONG)
+                    .setAction("Open Settings") {
+                        startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
+                    }.show()
             }
 
         }.on(Socket.EVENT_RECONNECT) {
             try {
                 mSocket?.connect()
                 mSocket?.emit(
-                    "patientsPoolForDoctor", jsonObject1
+                    "patientsPoolForDoctor", jsonObjectToken
                 )
-                activity?.runOnUiThread{
-                    binding.animationView.visibility= VISIBLE
+                activity?.runOnUiThread {
+                    if (InternetConnectivity.isNetworkAvailable(requireContext()) == false) {
+                        offlineCase()
+                    } else {
+                        binding.animationView.setAnimation(R.raw.heartbeat_loading)
+                        binding.animationView.playAnimation()
+                        binding.animationView.repeatMode
+                        binding.animationView.visibility = VISIBLE
+                        binding.openSettings.visibility = GONE
+                    }
                 }
             } catch (e: URISyntaxException) {
                 e.printStackTrace()
@@ -167,13 +214,12 @@ class DiagnosisPendingFragment : Fragment() {
 
         try {
             mSocket?.emit(
-                "patientsPoolForDoctor", jsonObject1
+                "patientsPoolForDoctor", jsonObjectToken
             )
         } catch (e: URISyntaxException) {
             e.printStackTrace()
         }
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
--- app/src/main/java/com/dsckiet/covidtracker/utils/ExtraUtils.kt
+++ app/src/main/java/com/dsckiet/covidtracker/utils/ExtraUtils.kt
@@ -2,10 +2,8 @@ package com.dsckiet.covidtracker.utils
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.util.Log
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 
 @SuppressLint("LogNotTimber")
 fun logs(msg: String) = Log.d("test->", " $msg")
